5aec4dcecc827bb66cb3e9a04d0c80b58eda75fd
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2021-12-06 23:24:42 +0600

    update

--- Documentation/app/Http/Controllers/HomeController.php
+++ Documentation/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -25,4 +26,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+    public function queryBuilder(){
+        $user= DB::table('users')->get();
+        return view('welcome',['users'=>$user]);
+    }
+
 }
