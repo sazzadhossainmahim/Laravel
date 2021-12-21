18c36c168cb3f41614bca3429b707c3222cf41f0
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2021-12-21 21:28:06 +0600

    update composer

---
+++ SchoolManagementSystem/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+
+}
